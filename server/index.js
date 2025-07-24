d6e8452146b57eb5047a80039a0ad1dc58b078d1
Anuxrag <notanurag@icloud.com>
2025-07-24 17:04:25 +0530

    connect backend and frontend

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import {router} from "./route.js";
+import cors  from 'cors';
 
 const app = express();
 
 const PORT=8000
 
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/',(req,res)=>{
-    res.send('This is HomePage');
+    res.send({message:'Hello from server side'});
 });
 
 
-app.use('/',router)
+app.use('/',router);
+
+console.log("hai from server side");
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}\n`);
